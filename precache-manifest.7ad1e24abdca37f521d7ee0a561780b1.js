862a252a2f6b100aab6bffd3b1f0eb60772fcd69
Akhil <geoakhil@gmail.com>
2020-07-14 15:10:41 +0530

    Updates

---
+++ precache-manifest.7ad1e24abdca37f521d7ee0a561780b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae893f88a999942d246a23fb8263ba7f",
+    "url": "/mini-folio/index.html"
+  },
+  {
+    "revision": "7be9bc5dcd1b4855294c",
+    "url": "/mini-folio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "08cc2686f28daebb56fd",
+    "url": "/mini-folio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7be9bc5dcd1b4855294c",
+    "url": "/mini-folio/static/js/2.daf83d29.chunk.js"
+  },
+  {
+    "revision": "7694ed5eff56d74dfc4b6ac3fc78a12b",
+    "url": "/mini-folio/static/js/2.daf83d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08cc2686f28daebb56fd",
+    "url": "/mini-folio/static/js/main.4a7486d2.chunk.js"
+  },
+  {
+    "revision": "c83b86e23c05e3860848",
+    "url": "/mini-folio/static/js/runtime-main.051c8e0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mini-folio/static/media/logo.5d5d9eef.svg"
+  }
+]);
